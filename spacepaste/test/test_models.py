b299a2d8695e6674d256b1d0f40a8eaa65b5dbfd
Andreas Stuehrk <andy@hammerhartes.de>
2011-03-13 04:59:47 +0100

    More tests.

--- spacepaste/test/test_models.py
+++ spacepaste/test/test_models.py
@@ -10,11 +10,28 @@ class ModelsTest(unittest.TestCase):
         db.session.add(self.private_parent)
         db.session.commit()
 
-    def test_private_reply(self):
-        # XXX Ideally, there should be some timeout or something
-        paste = models.Paste("", "text", self.private_parent, None, True)
+    def add_private(self, parent=None):
+        paste = models.Paste("", "text", parent, None, True)
         db.session.add(paste)
         db.session.commit()
+        return paste
+
+    def test_private_reply(self):
+        # XXX Ideally, there should be some timeout or something
+        self.add_private(self.private_parent)
+
+    def test_private_pastes_not_in_find_all(self):
+        pastes = list(models.Paste.find_all())
+        self.assertEqual(pastes, [])
+
+    def test_get_wrong_identifier(self):
+        paste = self.add_private(self.private_parent)
+        self.assertEqual(models.Paste.get(paste.paste_id), None)
+
+    def test_resolve_root_private_no_parent(self):
+        root = models.Paste.resolve_root(self.private_parent.identifier)
+        self.assertEqual(root.identifier, self.private_parent.identifier)
+
 
 if __name__ == "__main__":
     unittest.main()
